116342ffe6471c4bdcd9c4008bdcfc17511a883b
Kailey <k.kotynski@gmail.com>
2023-03-31 12:46:20 -0700

    Initial commit, added styles/imgs, poke module

---
+++ src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonDetailsComponent } from '../pokemon-details/pokemon-details.component';
+
+@NgModule({
+  declarations: [PokemonDetailsComponent],
+  imports: [CommonModule],
+})
+export class PokemonModule {}
